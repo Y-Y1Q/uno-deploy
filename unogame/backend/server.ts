5fdcbf86651e68d1e5994ccd7130e52fc995a5ce
sunnytan53 <27101659+sunnytan53@users.noreply.github.com>
2024-04-01 21:21:33 -0700

    backend server route fix

--- unogame/backend/server.ts
+++ unogame/backend/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import * as Routes from "./routes";
-import * as TestRoutes from "./routes/test";
+import * as TestRoutes from "./test_pages/test_routes";
 import * as Session from "./middleware/session";
 import { isAuthenticated } from "./middleware/check_auth";
 import { setUpDevelopmentEnvironment } from "./utilities/setup_development_environment";
@@ -37,11 +37,14 @@ app.use(Session.config);
 
 // Todo, learn socket.io
 
-// Temp routes for testing
 app.use(Routes.user);
+app.use(Routes.game);
+
+// test routes
 app.use(TestRoutes.root);
 app.use("/lobby", isAuthenticated, TestRoutes.lobby);
 app.use("/time", isAuthenticated, TestRoutes.logTime);
+app.use("/roomcreate", TestRoutes.game);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
