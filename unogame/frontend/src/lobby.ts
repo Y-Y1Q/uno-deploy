ba36e70f206ef169ba2234cfea532c14c7a0e8b0
Sidney <sid.14boys@yahoo.fr>
2024-04-05 11:04:21 -0700

    Update lobby confirm selection button to play game button

--- unogame/frontend/src/lobby.ts
+++ unogame/frontend/src/lobby.ts
@@ -110,7 +110,7 @@ const renderLobbyPage = () => {
                                 <p> Waiting for other players to join</p>
                             </div>
                             <div>
-                                <button class="bg-blue-500 text-white px-4 py-2 rounded mt-4 w-full" id="confirmSelectionButton">Confirm Selection</button>
+                                <button class="bg-blue-500 text-white px-4 py-2 rounded mt-4 w-full" id="playGameButton">Play Game</button>
                                 <button class="bg-gray-500 text-white px-4 py-2 rounded mt-4 w-full" id="goBackButton">Go Back</button>
                             </div>
                         </div>
