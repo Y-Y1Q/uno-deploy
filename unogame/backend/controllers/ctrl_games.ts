dfd1c23df436a2fb265f95f384483230a87301e5
sunnytan53 <haoyuan.t@outlook.com>
2024-04-07 22:02:13 -0700

    hotfix on previous commit

--- unogame/backend/controllers/ctrl_games.ts
+++ unogame/backend/controllers/ctrl_games.ts
@@ -1,5 +1,5 @@
 import { createGameroom } from "./games/create_room";
 import { getAllGamerooms } from "./games/get_all_rooms";
-import { getGameroomsByName } from "./games/get_rooms_by_names";
+import { getGameroomsByName } from "./games/get_rooms_by_name";
 
 export { createGameroom, getAllGamerooms, getGameroomsByName };
