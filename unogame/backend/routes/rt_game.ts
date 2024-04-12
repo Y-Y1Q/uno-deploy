87ff5f733f8b835b92644a5c4daf0bb125d6ff8e
Y-Y1Q <sh4bee@outlook.com>
2024-04-12 02:53:06 -0700

    chore: BE - add todo

--- unogame/backend/routes/rt_game.ts
+++ unogame/backend/routes/rt_game.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import { isUserInGame } from "../middleware/check_auth";
+import { chatController } from "../controllers/ctrl_chat";
 import * as Games from "../controllers/ctrl_games";
 
 const router = express.Router();
 
-router.post("/creategameroom", Games.createGameroom);
-router.post("/getallgamerooms", Games.getAllGamerooms);
-router.post("/getgameroomsbyname", Games.getGameroomsByName);
-router.post("/joingameroom", Games.joinGameroom);
-router.post("/quitgameroom", Games.quitGameroom);
+router.post("/:id/chat", isUserInGame, chatController);
+
+router.post("/create", Games.createGameroom);
+router.get("/get-all-games", Games.getAllGamerooms);
+router.get("/get-game-by-name", Games.getGameroomsByName);
+router.post("/:id/join", Games.joinGameroom);
+router.post("/:id/quit", isUserInGame, Games.quitGameroom);
+
+// Todo
+// router.post("/:id/start", Games.);
+// router.post("/:id/draw-card", Games.);
+// router.post("/:id/play-card", Games.);
+// router.post("/:id/say-uno", Games.);
 
 export default router;
--- unogame/backend/routes/rt_lobby.ts
+++ unogame/backend/routes/rt_lobby.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import { chatController } from "../controllers/ctrl_chat";
 
 const router = express.Router();
 
+router.post("/chat", chatController);
+
 // Todo
+// router.get("/get-games-joined", Lobby. );
+// router.get("/get-games-can-join", Lobby. );
+// router.get("/get-players-list", Lobby. ); ?? To be discussed later
+// router.post("/send-inv", Lobby. ); ?? To be discussed later
 
 export default router;
