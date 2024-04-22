c3c253f01934852a50b83ada5eb955942ac7e286
Y-Y1Q <sh4bee@outlook.com>
2024-04-21 23:10:34 -0700

    feat: BE - lobby controller

---
+++ unogame/backend/controllers/ctrl_lobby.ts
@@ -0,0 +1,6 @@
+import { getPlayersList } from "./lobby/get_players";
+import { getGamesCanJoin } from "./lobby/get_games_can_join";
+import { getGamesJoined } from "./lobby/get_games_joined";
+import { sendInvitation } from "./lobby/send_inv";
+
+export {getPlayersList, getGamesCanJoin, getGamesJoined, sendInvitation,}
---
+++ unogame/backend/controllers/lobby/get_players.ts
@@ -0,0 +1,23 @@
+import * as UserDB from "../../db/db_users";
+import HttpCode from "../../utilities/http_code";
+
+// delete comment later
+// get players list with id, username, fullname
+
+const getPlayersList = async (req, res) => {
+  
+try{
+    const playersList = UserDB.getAllUsers;
+    return res
+    .status(HttpCode.OK)
+    .json(playersList);
+
+}catch(err){
+    console.log(err);
+    return res
+      .status(HttpCode.InternalServerError)
+      .json({ error: "Internal server error: " + err });
+}
+};
+
+export { getPlayersList };
