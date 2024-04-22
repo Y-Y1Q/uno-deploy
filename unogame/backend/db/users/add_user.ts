5554f1d6750ef869066775d0609857be0250302f
Y-Y1Q <sh4bee@outlook.com>
2024-04-22 00:37:43 -0700

    chore: BE - remove unused files

--- unogame/backend/db/users/add_user.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "../db_connection";
-
-const ADD_USER =
-  "INSERT INTO users (username, password, fullname) VALUES ($1, $2, $3) RETURNING id, username, fullname";
-
-const addUser = (username, password, fullname) => db.one(ADD_USER, [username, password, fullname]);
-
-export { addUser };
--- unogame/backend/db/users/get_user.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "../db_connection";
-
-const GET_USER = "SELECT * FROM users WHERE username=$1";
-
-const getUser = (username) => {
-  return db.one(GET_USER, [username]);
-};
-
-export { getUser };
