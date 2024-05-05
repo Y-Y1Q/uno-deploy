06e7d8fcbbf47a1bf6d5ee115ae493f89c3c4607
Y-Y1Q <sh4bee@outlook.com>
2024-05-04 19:45:00 -0700

    chore: add config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./unogame/frontendV2/views/*.ejs"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/forms")],
+};
