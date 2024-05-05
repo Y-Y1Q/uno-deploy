1981bc985a7b9e8ad280bb6ef9e4636726a83d2f
Y-Y1Q <sh4bee@outlook.com>
2024-05-04 21:53:40 -0700

    chore: edit socket handler

--- unogame/backend/middleware/socket_connect.ts
+++ unogame/backend/middleware/socket_connect.ts
@@ -1,35 +1,32 @@
 const handleSocketConnection = (socket) => {
-  //  Adds the socket to a room identified by the user's ID
+  // Check if the socket has a user session
   if (socket.request.session.user !== undefined) {
+    const { request } = socket;
+
+    // Middleware to reload session to ensure reliability
+    socket.use((_, next) => {
+      request.session.reload((error) => {
+        if (error) {
+          socket.disconnect(); // Disconnect socket on session reload error
+        } else {
+          next(); // Continue with socket connection
+        }
+      });
+    });
+
+    // Join the socket to a room identified by the user's ID
     socket.join(socket.request.session.user.id);
-    console.log(`Username-${socket.request.session.user.username} UserId-${socket.request.session.user.id} connected`);
+    console.log(
+      `Username-${socket.request.session.user.username} UserId-${socket.request.session.user.id} connected`
+    );
+
+    // Handle socket disconnection
+    socket.on("disconnect", () => {
+      console.log(
+        `Username-${socket.request.session.user.username} UserId-${socket.request.session.user.id} disconnected`
+      );
+    });
   }
-
-
-  /* 
-  
-  TESTing - will update later
-  
-  
-  */
-
-  // Joins the socket to a room identified by the  parameter :id
-  // For example /game/25231    /game/11  /game/4214 /game/Dsge%2sd
-
-
-  // if (socket.handshake.query !== undefined) {
-  //   // socket.join(socket.handshake.query.id);
-  //   const {socketQuery} =  socket.handshake.query;
-  //   console.log(`Socket query: ${socketQuery}`);
-  // }
-
-  // // Listen for 'chat:message' events
-  // socket.on('chat:message', (data) => {
-  //   // Broadcast the message to the room (in our case, lobby because '0')
-  //   if (socket.request.session.user !== undefined) {
-  //     socket.to(0).emit('chat:message', data);
-  //   }
-  // });
 };
 
 export { handleSocketConnection };
