a8995bf783e20741157e0e557253343a81b22605
Y-Y1Q <sh4bee@outlook.com>
2024-05-06 16:19:50 -0700

    chore: edit routes for BE test page

--- unogame/backend/routes/rt_test.ts
+++ unogame/backend/routes/rt_test.ts
@@ -1,15 +1,12 @@
 import express from "express";
 
 import * as Chat from "../controllers/ctrl_chat";
+import { isAuthenticated } from "../middleware/check_auth";
 import * as Session from "../middleware/session";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.render("test/test_root");
-});
-
-router.get("/test/game/:id", (req, res) => {
+router.get("/test/game/:id", isAuthenticated, (req, res) => {
   const { id } = req.params;
 
   const { username: currentUser, id: currentUserId } =
@@ -18,22 +15,22 @@ router.get("/test/game/:id", (req, res) => {
   res.render("test/test_game", { id, currentUser, currentUserId });
 });
 
-router.get("/test/lobby", (req, res) => {
+router.get("/test", isAuthenticated, (req, res) => {
   const { username: currentUser, id: currentUserId } =
     Session.getCurrentUser(req);
 
   res.render("test/test_lobby", { currentUser, currentUserId });
 });
 
-router.get("/test/m2-chat", (req, res) => {
+router.get("/test/m2-chat", isAuthenticated, (req, res) => {
   const { username: currentUser, id: currentUserId } =
     Session.getCurrentUser(req);
 
   res.render("test/test_m2_chat", { currentUser, currentUserId });
 });
 
-router.post("/test/chat", Chat.sendMessage);
-router.post("/test/:id/chat", Chat.sendMessage);
-router.post("/test/:id/inv", Chat.sendInvitation);
+router.post("/test/chat", isAuthenticated, Chat.sendMessage);
+router.post("/test/:id/chat", isAuthenticated, Chat.sendMessage);
+router.post("/test/:id/inv", isAuthenticated, Chat.sendInvitation);
 
 export default router;
