c68d6b71b4b16b2fddcb152b7d4b82e06f47c8cc
Y-Y1Q <sh4bee@outlook.com>
2024-05-05 19:56:23 -0700

    chore: format console log for request

--- unogame/backend/middleware/timestamp.ts
+++ unogame/backend/middleware/timestamp.ts
@@ -1,6 +1,8 @@
 const requestTime = (req, res, next) => {
   const timestamp = new Date().toLocaleString();
-  console.log(`\nRequest received at ${timestamp}\n${req.method} ${req.url}`);
+  console.log(
+    `\nRequest \x1b[32m\x1b[1m ${req.method} ${req.url} \x1b[0m received at ${timestamp}`
+  );
   next();
 };
 
